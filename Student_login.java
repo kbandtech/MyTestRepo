c7ba95c2997b62380e519c719ab262ef08136740
kk <kk@gmail.com>
2024-08-03 06:49:28 +0000

    java commit

---
+++ Student_login.java
@@ -0,0 +1,13 @@
+public class Student_login
+{
+	public static void main(String args[])
+	{
+		System.out.println("Vansro Technologies");
+		System.out.println("Vansro Technologies");
+		System.out.println("Vansro Technologies");
+		System.out.println("Vansro Technologies");
+
+	}
+
+}
+
